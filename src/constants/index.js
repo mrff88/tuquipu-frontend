24cc98281faf96f0b154cfc6fcfaad5fa9ab0690
mrff88 <mrff88@gmail.com>
2022-05-16 08:39:51 -0500

    feat: separate constants for endpoints, formValidation, initialFormState and style in constants folder

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 import ENDPOINTS from './endpoints';
 import INITIAL_FORM_STATES from './initialFormStates';
 import FORM_VALIDATION_SCHEMAS from './formValidationSchemas';
+import STYLES from './styles';
 
-export { ENDPOINTS, INITIAL_FORM_STATES, FORM_VALIDATION_SCHEMAS };
+export { ENDPOINTS, INITIAL_FORM_STATES, FORM_VALIDATION_SCHEMAS, STYLES };
