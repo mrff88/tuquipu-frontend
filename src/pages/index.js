58325fe1f132424dfe3eea2b9a5dc96279a55f7f
mrff88 <mrff88@gmail.com>
2022-05-16 09:25:20 -0500

    feat: update routing with client and user pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Login from './Login';
 import Register from './Register';
+import Users from './Users';
+import Clients from './Clients';
 
-export { Login, Register };
+export { Login, Register, Users, Clients };
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { NoAuthLayout } from '../components/layouts';
-import { Login, Register } from '../pages';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Notifications } from '../components/Notifications';
+import {
+  AdminRoutesGuard,
+  NoAuthLayout,
+  WithAuthLayout,
+} from '../components/layouts';
+import { Clients, Login, Register, Users } from '../pages';
 
 function App() {
   return (
-    <Routes>
-      <Route element={<NoAuthLayout />}>
-        <Route index element={<Login />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route element={<WithAuthLayout />}>
+          <Route element={<AdminRoutesGuard />}>
+            <Route index element={<Users />} />
+            <Route exact path="/usuarios" element={<Users />}></Route>
+          </Route>
+          <Route exact path="/clientes" element={<Clients />}></Route>
+          <Route path="/not-found" element={<h1>404 Not Found</h1>} />
+          <Route path="*" element={<Navigate to="/not-found" replace />} />
+        </Route>
+        <Route element={<NoAuthLayout />}>
+          <Route index element={<Login />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Route>
+      </Routes>
+      <Notifications />
+    </>
   );
 }
 
