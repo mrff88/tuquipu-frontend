df0de473569d3677a1f05f69ffe5270a73ca2dcb
mrff88 <mrff88@gmail.com>
2022-05-22 14:26:54 -0500

    feat: add deviceDataAdapter util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import parseJWT from './parseJWT';
 import userDataAdapter from './dataAdapters/userDataAdapter';
 import clientDataAdapter from './dataAdapters/clientDataAdapter';
+import deviceDataAdapter from './dataAdapters/deviceDataAdapter';
 
-export { parseJWT, userDataAdapter, clientDataAdapter };
+export { parseJWT, userDataAdapter, clientDataAdapter, deviceDataAdapter };
