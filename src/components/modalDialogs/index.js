8a6a76381c6e7c2cd386eee6e8c8b7e92b0073c7
mrff88 <mrff88@gmail.com>
2022-05-22 18:05:35 -0500

    feat: add serviceCancel and ServiceFinish modalDialog component

--- src/components/modalDialogs/index.js
+++ src/components/modalDialogs/index.js
@@ -2,5 +2,14 @@ import UserStateChange from './UserStateChange';
 import UserInfo from './UserInfo';
 import ClientInfo from './ClientInfo';
 import DeviceInfo from './DeviceInfo';
+import ServiceCancel from './ServiceCancel';
+import ServiceFinish from './ServiceFinish';
 
-export { UserStateChange, UserInfo, ClientInfo, DeviceInfo };
+export {
+  UserStateChange,
+  UserInfo,
+  ClientInfo,
+  DeviceInfo,
+  ServiceCancel,
+  ServiceFinish,
+};
